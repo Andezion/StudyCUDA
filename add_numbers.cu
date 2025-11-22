af514616868dca37fa12b5b18f5fba3ece95664e
Andezion <245122@edu.p.lodz.pl>
2025-11-22 17:47:18 +0100

    cool add func

--- add_numbers.cu
+++ add_numbers.cu
@@ -1,34 +1,47 @@
-#include <iostream>
-#include <cuda_runtime.h>
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <cstdio>
 
-__global__ void add(const int a, const int b, int *c)
+__global__ void add( const int *a, const int *b, int *c )
 {
-    printf("Thread %d in block %d: %d + %d = %d\n", 
-           threadIdx.x, blockIdx.x, a, b, a + b);
-    *c = a + b;
+    *c = *a + *b;
 }
+
+__global__ void sub( const int *a, const int *b, int *c)
+{
+    *c = *a - *b;
+}
+
+__global__ void mul( const int *a, const int *b, int *c)
+{
+    *c = *a * *b;
+}
+
+
 
 int main()
 {
     int a, b, c;
-    int *dev_c;
-    
-    std::cout << "Enter first number: ";
-    std::cin >> a;
-    std::cout << "Enter second number: ";
-    std::cin >> b;
-    
-    cudaMalloc(reinterpret_cast<void **>(&dev_c), sizeof(int));
 
-    add<<<1, 1>>>(a, b, dev_c);
+    int *dev_a, *dev_b, *dev_c;
+    constexpr int size = sizeof( int );
 
-    cudaDeviceSynchronize();
-    
-    cudaMemcpy(&c, dev_c, sizeof(int), cudaMemcpyDeviceToHost);
-    
-    std::cout << "\nResult: " << a << " + " << b << " = " << c << std::endl;
-    
-    cudaFree(dev_c);
-    
+    cudaMalloc( reinterpret_cast<void **>(&dev_a), size );
+    cudaMalloc( reinterpret_cast<void **>(&dev_b), size );
+    cudaMalloc( reinterpret_cast<void **>(&dev_c), size );
+
+    a = 2;
+    b = 7;
+
+    cudaMemcpy( dev_a, &a, size, cudaMemcpyHostToDevice);
+    cudaMemcpy( dev_b, &b, size, cudaMemcpyHostToDevice);
+
+    add<<< 1, 1 >>>( dev_a, dev_b, dev_c );
+    cudaMemcpy( &c, dev_c, size, cudaMemcpyDeviceToHost);
+
+    printf("%d + %d = %d\n", a, b, c);
+    cudaFree( dev_a );
+    cudaFree( dev_b );
+    cudaFree( dev_c );
     return 0;
 }
